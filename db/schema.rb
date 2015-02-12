45ad5626d5ab70a4d0327110e63f93ba741fe27e
Afmellado <fitobass@gmail.com>
2015-02-12 12:05:04 -0300

    views and strong parameters in devise controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212142610) do
+ActiveRecord::Schema.define(version: 20150212143955) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150212142610) do
     t.integer  "user_id"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "permalink"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -55,7 +56,6 @@ ActiveRecord::Schema.define(version: 20150212142610) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
-    t.string   "permalink"
     t.text     "bio"
     t.string   "twitter"
     t.string   "facebook"
